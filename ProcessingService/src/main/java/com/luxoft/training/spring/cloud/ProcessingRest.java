0511d8feecdf377629450b52b30383032506f4b4
Nickolay Erokhin <NVErokhin@sberbank.ru>
2020-11-25 13:13:10 +0300

    add hystrix and turbine

--- ProcessingService/src/main/java/com/luxoft/training/spring/cloud/ProcessingRest.java
+++ ProcessingService/src/main/java/com/luxoft/training/spring/cloud/ProcessingRest.java
@@ -1,5 +1,6 @@
 package com.luxoft.training.spring.cloud;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,6 +52,21 @@ public class ProcessingRest {
         }
         return map;
     }
+
+    @HystrixCommand(fallbackMethod = "testFallback")
+    @RequestMapping("/test")
+    public String testHystrix(Boolean fail) {
+        if (Boolean.TRUE.equals(fail)) {
+            throw new RuntimeException();
+        }
+        return "OK";
+    }
+
+    private String testFallback(Boolean fail) {
+        return "FAILED";
+    }
+
+
 
 
 }
